ddf6fa24aff5b3fc2ecb43799920ca10827a7158
AnatolyRM <1neytron@gmail.com>
2022-11-03 19:46:46 +0300

    init: add task 04-copy-directory - completed!

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,15 +5,15 @@ async function copyDir() {
 
   try {
     await fsPromise.access(path.join(__dirname, 'files-copy'), fs.F_OK);
-    const del = await fsPromise.rm(path.join(__dirname, 'files-copy'), { recursive: true });
-  } catch { console.log(`--- Ствараем тэчку ---`)}
+    const filesdel = await fsPromise.rm(path.join(__dirname, 'files-copy'), { recursive: true });
+  } catch { console.log(`--- Ствараем тэчку files-copy ---`)}
 
   try {
     const filesCopy = await fsPromise.mkdir(path.join(__dirname, 'files-copy'), { recursive: true })
     const files = await fsPromise.readdir(path.join(__dirname, 'files'), { withFileTypes: true })
     for (const file of files) {
-      console.log(file)
       await fsPromise.copyFile(path.join(__dirname, 'files', `${file.name}`), path.join(__dirname, 'files-copy', `${file.name}`))
+      console.log(`--- Ствараем копію файла ${file.name}} ---`)
     }
   } catch (error) { console.error(error) }
 }
